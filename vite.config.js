cf60b4681ee98cce6004733eac1bc424e9335b34
silasly <gosilasbot@gmail.com>
2026-08-18 22:50:15 +0200

    fix: inject data-cfasync=false to prevent Cloudflare Rocket Loader script delays

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Custom plugin to ensure Rocket Loader does not alter module scripts
+function rocketLoaderBypass() {
+  return {
+    name: 'rocket-loader-bypass',
+    transformIndexHtml(html) {
+      return html.replace(/<script /g, '<script data-cfasync="false" ')
+    },
+  }
+}
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), rocketLoaderBypass()],
   base: './',
   server: {
     host: '0.0.0.0',
